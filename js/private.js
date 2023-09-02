f5cee947286d69ce6d8db32825e55ea7273f672c
giangson209 <ledinhson209@gmail.com>
2023-09-02 10:43:37 +0700

    push

--- js/private.js
+++ js/private.js
@@ -3,4 +3,16 @@ $( document ).ready(function() {
     $('.header__button').click(function(){
         $('.menu-nav, .header__button-line').toggleClass('active');
     });
+
+    $('.banner-slick-slider').slick({
+      slidesToShow: 1,
+      dots:false,
+    });
+
+    $('.list-image__list').slick({
+      arrows: false,
+      slidesToShow: 4,
+      dots:false,
+      centerMode: true,
+    });
 });
