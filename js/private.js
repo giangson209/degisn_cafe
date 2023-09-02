ccd6e2c9f572b14e7491c282a647cfd1fb0ed4d6
giangson209 <ledinhson209@gmail.com>
2023-09-02 23:04:35 +0700

    update

--- js/private.js
+++ js/private.js
@@ -1,18 +1,51 @@
 
 $( document ).ready(function() {
+
+    $(window).scroll(function() {
+       var headerH = $('.header').outerHeight(true);
+       console.log(headerH);
+       var scrollVal = $(this).scrollTop();
+        if ( scrollVal > headerH ) {
+            $('.header').addClass('fixmenu');
+        } else {
+            $('.header').removeClass('fixmenu');
+        }
+    });
+
+
+
     $('.header__button').click(function(){
         $('.menu-nav, .header__button-line').toggleClass('active');
     });
 
     $('.banner-slick-slider').slick({
+      autoplay:true,
       slidesToShow: 1,
       dots:false,
+      fade: true,
+      cssEase: 'linear'
     });
 
     $('.list-image__list').slick({
-      arrows: false,
-      slidesToShow: 4,
-      dots:false,
-      centerMode: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 0,
+        slidesToShow: 4,
+        speed: 7000,
+        cssEase: "linear",
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+              }
+            }
+        ]
     });
 });
