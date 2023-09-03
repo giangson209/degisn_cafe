40563368e571702c9a4b4b7f9e3688a0adae8295
giangson209 <ledinhson209@gmail.com>
2023-09-03 23:22:31 +0700

    update

--- js/private.js
+++ js/private.js
@@ -17,16 +17,44 @@ $( document ).ready(function() {
         $('.menu-nav, .header__button-line').toggleClass('active');
     });
 
+    //scroll animation
+    jQuery(function($) {
+      var doAnimations = function() {
+        var offset = $(window).scrollTop() + $(window).height(),
+            $animatables = $('.concept__list-item-image');
+            $animatables_delay = $('.concept__list-item-text');
+        if ($animatables.length == 0) {
+          $(window).off('scroll', doAnimations);
+        }
+        $animatables.each(function(i) {
+           var $animatable = $(this);
+          if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+                $animatable.addClass('animated');
+                setTimeout(function() { 
+                    $animatables_delay.addClass('animated');
+                }, 600);
+          } else {
+            $animatable.removeClass('animated'); 
+            $animatables_delay.removeClass('animated'); 
+          }
+        });
+      };
+      $(window).on('scroll', doAnimations);
+      $(window).trigger('scroll'); 
+    }); 
+
     // main slider
     let mainSlider = $('.banner-slick-slider')
     if($(mainSlider).length)  {
         $(mainSlider).slick({
             autoplay:true,
+            autoplaySpeed: 8000,
             slidesToShow: 1,
             dots:false,
             fade: true,
-            cssEase: 'linear'
-        });
+            cssEase: 'linear', 
+            speed: 5000,
+        }); 
     }
 
     // gerally slider
